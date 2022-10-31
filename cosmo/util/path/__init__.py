63d282d39586998e4ca2f4901a70d69ca766d5b0
Mateus Santos <a.mateussm1.5@gmail.com>
2022-10-31 10:49:05 -0300

    :sparkles: | Upgrade python requirements

--- cosmo/util/path/__init__.py
+++ cosmo/util/path/__init__.py
@@ -5,5 +5,5 @@ HEADERS_PATH: str = f'{ROOT_PATH}/data/json/table_headers.json'
 XLSX_PATH: str = f'{ROOT_PATH}/data/xlsx/data.xlsx'
 
 voice_models: dict[str, str] = {
-    'pt-br': f'{ROOT_PATH}/data/voice_model/pt-br/vosk-model-small-pt-0.3'
+    'pt-br': f'{ROOT_PATH}/data/voice_model/pt-br/vosk-model-pt-fb-v0.1.1-20220516_2113'
 }
